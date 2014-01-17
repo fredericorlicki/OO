727a100a6d430c4524d879bc186af04bf68c26a7
Mac Mob <Macmob@MacBook-Pro-de-Mac.local>
2014-01-17 17:09:28 +0100

    solution bank account

--- 03-Public-vs-Private/account.rb
+++ 03-Public-vs-Private/account.rb
@@ -1,59 +1,69 @@
-# This is how you define your own custom exception classes
+require_relative 'transaction'
+
 class DepositError < StandardError
 end
 
+class WithdrawError < StandardError
+end
+
 class BankAccount
+
+  attr_reader :name, :position, :transactions 
   
-  # Contract for the BankAccount class
-  # - you can access full owner's name and position, but partial IBAN
-  # - you cannot access full IBAN
-  # - you can print partial account infos
-  # - you can print transactions only with a password
-  # - you can withdraw or deposit money
-  
-  MIN_DEPOSIT =  100
-  
-  def initialize(name, iban, initial_deposit, password)
+  MIN_DEPOSIT = 100
+   
+ def initialize(name, iban, initial_deposit, password)
     raise DepositError, "Insufficient deposit" unless initial_deposit > MIN_DEPOSIT
-    @password = password
-    @transactions = []
-    @position = 0
-    @name, @iban = name, iban
+     @password = password
+     @transactions = []
+     @position = 0
+     @name, @iban = name, iban
     
     add_transaction(initial_deposit)
   end
     
   def withdraw(amount)
+    raise WithdrawError, "Insufficients funds" unless amount >= @position
+    add_transaction(-amount)
+    puts "You have withdrawn #{amount} euros" 
   end
   
   def deposit(amount)
+    raise DepositError, "Invalid amount" unless amount > 0
+    add_transaction(amount)
+    puts "you have made a deposit of #{amount}"
   end
   
   def transactions_history(args = {})
-    # Should print transactions, BUT NOT return the transaction array !
+    if args == {}
+      puts "No password given. Access denied"
+    else
+      if args[:password] == @password
+        puts @transactions
+      else
+        puts "wrong password"
+    end
+
   end
-  
+
+return nil 
+end
   def iban
-    # Partial getter (should hide the middle of the IBAN like FR14**************606)
+    @iban[0, 4] + "**************" + @iban[-3,3]
   end
   
   def to_s
-    # Method used when printing account object as string (also used for string interpolation)
+    "Owner: #{@name}\nIBAN: #{iban}\nCurrent amount: #{@position} euros"
   end
           
   private  
   
   def add_transaction(amount)
-    # Main account logic
-    # Should add the amount in the transactions array
-    # Should update the current position
+    @transactions << Transaction.new(amount)
+    @position += amount
   end
-    
-end
+end 
 
-# TESTING YOUR BANK ACCOUNT
-
-# In the outside world, create a new account for Bruce Lee
 account = BankAccount.new("Bruce Lee", "FR14-2004-1010-0505-0001-3M02-606", 200, "brucelit")
 
 # Accessible infos
